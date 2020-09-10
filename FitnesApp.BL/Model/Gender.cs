ea709b93177200abd214de15da8b994310c9e668
islamessi <islamessi@inbox.ru>
2020-09-10 15:51:26 +0300

    Создана модель пользователя.

---
+++ FitnesApp.BL/Model/Gender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FitnesApp.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название пола.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name"> Название пола. </param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException("Имя пола не может пыть пустым!", nameof(name));
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
